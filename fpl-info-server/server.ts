8b661f8690cd2c0a2a4d4099a344e46e4bd74326
Rohan <maharoh9841@gmail.com>
2023-11-04 14:43:37 +0545

    production build

--- fpl-info-server/server.ts
+++ fpl-info-server/server.ts
@@ -1,11 +1,13 @@
-import express from "express"
-import cors from "cors"
+import express from 'express'
+import cors from 'cors'
+import path, { dirname } from 'path'
+import { fileURLToPath } from 'url'
 import {
   bootstrapRoutes,
   entryRoutes,
   fixtureRoutes,
   leagueRoutes,
-} from "./api/index.js"
+} from './api/index.js'
 
 const PORT = process.env.PORT || 5172
 
@@ -13,12 +15,22 @@ const app = express()
 
 app.use(cors())
 
-app.use("/api/bootstrap", bootstrapRoutes)
-app.use("/api/fixtures", fixtureRoutes)
-app.use("/api/entry", entryRoutes)
-app.use("/api/league", leagueRoutes)
-app.get("/", (req, res) => {
-  res.send("<h1>FPL API Server is Running...</h1>")
+app.use(express.static('./fpl-info/dist'))
+
+app.use('/api/bootstrap', bootstrapRoutes)
+app.use('/api/fixtures', fixtureRoutes)
+app.use('/api/entry', entryRoutes)
+app.use('/api/league', leagueRoutes)
+app.get('/api', (req, res) => {
+  res.send('<h1>FPL API Server is Running...</h1>')
+})
+
+app.get('*', (req, res) => {
+  const filePath = import.meta.resolve('../fpl-info', import.meta.url)
+  const __filename = fileURLToPath(filePath)
+  const __dirname = dirname(__filename)
+  // res.send(path.resolve(__dirname, 'dist', 'index.html'))
+  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'))
 })
 
 app.listen(PORT, () => {
--- fpl-info/src/components/app/MyTeam/ElementDetail.tsx
+++ fpl-info/src/components/app/MyTeam/ElementDetail.tsx
@@ -1,5 +1,5 @@
-import { PLAYER_IMG_URL } from "@/lib/app-utils/constants"
-import { EntryPickElement } from "@/lib/app-utils/hook"
+import { PLAYER_IMG_URL } from '@/lib/app-utils/constants'
+import { EntryPickElement } from '@/lib/app-utils/hook'
 
 type ElementDetailProps = {
   element: EntryPickElement
@@ -10,14 +10,14 @@ const ElementDetail = ({ element }: ElementDetailProps) => {
     <div className='flex flex-col items-center justify-center'>
       <img
         src={`${PLAYER_IMG_URL}p${element.element_detail?.photo
-          ?.split(".")
+          ?.split('.')
           .at(0)}.png`}
-        alt={element.element_detail?.web_name ?? ""}
+        alt={element.element_detail?.web_name ?? ''}
         className='w-20 aspect-[1/1.25]'
       />
       <div className='flex flex-col w-28'>
         <h1 className='text-sm font-bold bg-secondary-foreground text-slate-100 text-center'>
-          {element.element_detail?.web_name}{" "}
+          {element.element_detail?.web_name}{' '}
           {(element.is_captain && (
             <span className='rounded-full bg-slate-200 text-slate-800 px-1 ml-1'>
               C
@@ -29,9 +29,9 @@ const ElementDetail = ({ element }: ElementDetailProps) => {
               </span>
             ))}
         </h1>
-        <h2 className='text-sm bg-gray-300  text-center'>
+        <h2 className='text-sm bg-gray-300  text-center font-semibold'>
           {element.element_detail &&
-            (element.in_team === "regular"
+            (element.in_team === 'regular'
               ? element.element_detail.event_points * element.multiplier
               : element.element_detail.event_points)}
         </h2>
